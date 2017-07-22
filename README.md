06b3c7d5978f3bf2717e2c066f07e4b8b0b5019d
stephen chaffins <stephenchaffins@gmail.com>
2017-07-22 01:57:32 -0400

    Publishing
    Publishing

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# VDS-to-cPanel-Migration-Scripts
+Packaging Script : vds2cp.sh [ To be ran on the VDS ]
+-----------------------------------------------
+This is very basic. Login to the vds, download this file.
+
+    wget --no-check-certificate https://raw.githubusercontent.com/stephenchaffins/VDS-to-cPanel-Migration-Scripts/master/vds2cp.sh; chmod 755 vds2cp.sh
+
+
+To execute it just run:
+
+    ./vds2cp.sh
+
+It is very important that I should mention, this is ran INSIDE of the VDS, as the VDS user.
+
+Logs for everything this script has done is at /var/log/vds2cp.log
+
+
+
+
+
+Restoration Script : vds2cp_restore.sh [ To be ran on cPanel ]
+-----------------------------------------------
+1. Next is the restoration script. This is a bit more complicated to run, slightly, and requires some arguments. This script is ran on the cPanel server.
+
+2. rsync the file (/root/vds2cp_restore_USERNAME.tar.gz) from the VDS server to the cPanel server you plan on restoring to. This has to be at /root/vds2cp_restore_USERNAME.tar.gz on the cPanel server too. From the remote server
+
+3. SKIP STEP 3 FOR NOW, AS THE SCRIPT NOW CREATES THE CPANELACCOUNT.
+
+4. Download this vds2cp_restore.sh from this github repo.
+
+        wget --no-check-certificate https://raw.githubusercontent.com/stephenchaffins/VDS-to-cPanel-Migration-Scripts/master/vds2cp_restore.sh; chmod 755 vds2cp_restore.sh
+
+5. This must be executed with bash, not sh.
+
+6. This requires that you input the VDS username, then the cPanel username, and the password within single quotes. It will look like this:
+
+        ./vds2cp_restore.sh VDSUSERNAME CPANELUSERNAME 'password'
+
+This should do everything you need. You can remove the /root/vds2cp_restore_USERNAME.tar.gz file once everything looks good.
+
+Logs for everything the script has done is at /home/USERNAME/vds2cp.log
---
+++ vds2cp.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# @Author: Stephen Chaffins
+# @Date:   2017-02-28T13:48:19-05:00
+# @Email:  schaffins@techassets.com
+# @Project: VDS to cPanel Migration Scripts
+# @Filename: vds2cp.sh
+# @Last modified by:   Stephen Chaffins
+# @Last modified time: 2017-03-24T15:53:55-04:00
+# -----------------------------------------------------------------------------
+exec 2>> /var/log/vds2cp.log
+# -----------------------------------------------------------------------------
+# Using this line to set some basic vairables.
+# -----------------------------------------------------------------------------
+VDSUSER=`id|awk '{print $1}'|cut -d "(" -f 2|cut -d ")" -f 1`
+TODAY=`date +"%m-%d-%y"`
+WDIR=/root/"$TODAY"_"$VDSUSER"
+MDOM=`cat /bin/hostname | sed -n 2p | cut -d " " -f 2`
+IP=`cat /etc/hosts |awk '{print $1}'|tail -1`
+IFS=$'\n'
+
+# -----------------------------------------------------------------------------
+# Announce that the process has begun.
+# -----------------------------------------------------------------------------
+echo
+echo -e "\e[33m\e[1m Setting VDS user variable... \e[0m";sleep 1;
+echo
+
+# -----------------------------------------------------------------------------
+# Check if the /root directory exists.
+# -----------------------------------------------------------------------------
+if [ ! -d /root ]
+then
+  mkdir /root
+fi
+
+# -----------------------------------------------------------------------------
+# Making some of the text needed directories.
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Creating some directories to be used in the process... \e[0m"
+mkdir -p /root/"$TODAY"_"$VDSUSER"/{text_files,database_dumps};
+echo
+
+# -----------------------------------------------------------------------------
+# Get a list of email accounts
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Getting Email Account list... \e[0m"
+cat /etc/mail/virtusertable |awk '{print $1}'|grep -vE '\#|MAILER-DAEMON|postmaster|^$|root\@|ftp\@'| sed '/^@/ d' > "$WDIR"/text_files/"$VDSUSER"_mailusers;sleep 1;echo
+
+echo -e "\e[33m\e[1m Copying mailbox data... \e[0m";echo
+cp -a /var/spool/mail "$WDIR"/mailboxes
+
+# -----------------------------------------------------------------------------
+# Get the main domain
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Making main domain file... \e[0m"; echo
+grep -E 'ServerName|DocumentRoot' /etc/httpd/conf/httpd.conf | head -2|sed -e 's/.*Name\ //g' |sed -e 's/.*DocumentRoot\ //g'| sed '$!N;s/\n/ /'|awk ' { t = $1; $1 = $2; $2 = t; print; } ' > "$WDIR"/text_files/"$VDSUSER"_main_domain;
+
+
+
+# -----------------------------------------------------------------------------
+# Get the addon domains, and the subdomains.
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Getting Addon and Subdomain lists... \e[0m"; echo
+grep -E 'ServerName|DocumentRoot' /etc/httpd/conf/httpd.conf | sed 1,2d |sed -e 's/.*Name\ //g' |sed -e 's/.*DocumentRoot\ //g'| sed '$!N;s/\n/ /' |awk '$1 !~ (/.*\..*\./)' > "$WDIR"/text_files/"$VDSUSER"_addonsub_list;
+grep -E 'ServerName|DocumentRoot' /etc/httpd/conf/httpd.conf | sed 1,2d |sed -e 's/.*Name\ //g' |sed -e 's/.*DocumentRoot\ //g'| sed '$!N;s/\n/ /'|awk '$1 ~ (/.*\..*\./)' > "$WDIR"/text_files/"$VDSUSER"_subdomain_list;
+
+# -----------------------------------------------------------------------------
+# List aliased/parked domains that have the web directory set to /var/www/html
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Getting Parked/Aliased domains list... \e[0m"; echo
+cat "$WDIR"/text_files/"$VDSUSER"_addonsub_list | awk '$2 == "/var/www/html"'|awk '{print $1}' >> "$WDIR"/text_files/"$VDSUSER"_parked_domains
+
+
+# -----------------------------------------------------------------------------
+# Singling out the "default directory" parked domains.
+# -----------------------------------------------------------------------------
+excprk=`cat "$WDIR"/text_files/"$VDSUSER"_addonsub_list | awk '$2 != "/var/www/html"'|awk '{print $1}'`
+excadon=`cat "$WDIR"/text_files/"$VDSUSER"_addonsub_list | awk '$2 == "/var/www/html"'|awk '{print $1}'`
+cat /etc/mail/virtusertable |awk '{print $1}'|grep -vE '\#|^$'| sed '/^@/ d'|sed 's/.*@//'| sort -u >  "$WDIR"/text_files/"$VDSUSER"_all_domains
+cat "$WDIR"/text_files/"$VDSUSER"_all_domains |grep -Ev "$excprk|$MDOM|$excadon" >> "$WDIR"/text_files/"$VDSUSER"_parked_domains
+
+# -----------------------------------------------------------------------------
+# Gathering addon domains whos paths are not specifically /var/www/html.
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Cleaning up domain lists... \e[0m"
+cat "$WDIR"/text_files/"$VDSUSER"_addonsub_list |awk '$2 != "/var/www/html"' > "$WDIR"/text_files/"$VDSUSER"_addon_subdomains;
+echo
+
+# -----------------------------------------------------------------------------
+# This is going to start copying data for the addon domains.
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Copying addon domain and subdomain file data now... \e[0m"
+
+while read line
+do
+  d=`echo "$line" | awk '{print $1}'`
+  s=`echo "$line" | awk '{print $2}'`
+  mkdir -p /root/"$TODAY"_"$VDSUSER"/domain_files/$d/
+  cp -R "$s"/. /root/"$TODAY"_"$VDSUSER"/domain_files/$d/
+done < "$WDIR"/text_files/"$VDSUSER"_addon_subdomains;
+echo
+
+# -----------------------------------------------------------------------------
+# Copying subdomains data
+# -----------------------------------------------------------------------------
+
+while read sdcopy
+do
+  dcop=`echo "$sdcopy" | awk '{print $1}'`
+  scop=`echo "$sdcopy" | awk '{print $2}'`
+  mkdir -p /root/"$TODAY"_"$VDSUSER"/domain_files/$dcop/
+  cp -R "$scop"/. /root/"$TODAY"_"$VDSUSER"/domain_files/$dcop/
+done < "$WDIR"/text_files/"$VDSUSER"_subdomain_list;
+
+# -----------------------------------------------------------------------------
+# Copying the main domain data. This is messy and ugly, but there's no rsync.
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Copying main domain... \e[0m"
+grep -E 'ServerName|DocumentRoot' /etc/httpd/conf/httpd.conf | sed 1,2d |sed -e 's/.*Name\ //g' |sed -e 's/.*DocumentRoot\ //g'| sed '$!N;s/\n/ /' |awk '{print $2}' |awk -F "/" '{print $NF}' |grep -v '^html$' > "$WDIR"/text_files/tmp_excludes
+mkdir -p /root/"$TODAY"_"$VDSUSER"/domain_files/$MDOM
+ls /var/www/html/|grep -v '^fm$' |grep -v '^vdsbackup$'  |grep -vf "$WDIR"/text_files/tmp_excludes > "$WDIR"/text_files/mdom_exlist
+
+while read fline
+do
+  cp -R /var/www/html/$fline $WDIR/domain_files/$MDOM/
+done < "$WDIR"/text_files/mdom_exlist
+
+rm "$WDIR"/text_files/mdom_exlist
+rm "$WDIR"/text_files/tmp_excludes
+
+echo
+
+# -----------------------------------------------------------------------------
+# Find all existing MySQL databases. MySQL must be running.
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Getting list of MySQL databases and dumping them... \e[0m"
+
+if [ ! -f /usr/bin/mysql ];
+then
+  echo -e "\e[31m No databases to dump! \e[0m"
+else
+  ls -A1 /var/lib/mysql/ |grep -Ev "^mysql$|\.err|\.sock" > $WDIR/text_files/"$VDSUSER"_databases
+  for i in `cat "$WDIR"/text_files/"$VDSUSER"_databases`; do mysqldump $i > "$WDIR"/database_dumps/$i.sql;done
+fi
+echo
+
+# -----------------------------------------------------------------------------
+# This tars and gzips all thats been gathered (data and text files, and dumps.)
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Archiving and compressing everything thats been done...\e[0m \e[1m\e[41m BE PATIENT! \e[0m "; sleep 1
+while :; do
+  printf "."
+  sleep 1
+done &
+bgid=$!
+
+tar -C "$WDIR" -zcf /root/vds2cp_restore_"$VDSUSER".tar.gz . |grep -v "Removing leading"
+
+kill "$bgid"
+echo
+
+echo -e "\e[33m\e[1m Archiving of all data now complete..."; echo
+
+# -----------------------------------------------------------------------------
+# Remove all the backed up files that we tar'd up. Leaving the tar.gz will.
+# -----------------------------------------------------------------------------
+echo -e "\e[33m\e[1m Cleaning Up data and packaging files... \e[0m";echo
+rm -rf "$WDIR"
+
+# -----------------------------------------------------------------------------
+# Signaling the script has completed.
+# -----------------------------------------------------------------------------
+echo -e "\e[1m\e[44m vds2cp_restore_`echo $VDSUSER`.tar.gz file backed up to /root/vds2cp_restore_`echo $VDSUSER`.tar.gz \e[0m";sleep 1;
+echo
+
+echo -e "\e[1m\e[41m Exiting. Done. \e[0m";echo
+sleep 1;
+exit 0;
